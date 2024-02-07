7ed8badea155789fda9c449062afa08b16bb6b05
Mateusz <mateusz@mscode.pl>
2024-02-07 20:23:25 +0100

    .a

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,7 @@ sleep 1
 
 cd /home/container
 
-# Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
-
 ./run.sh
 
 # Run the Server
-${MODIFIED_STARTUP}
+${STARTUP}
